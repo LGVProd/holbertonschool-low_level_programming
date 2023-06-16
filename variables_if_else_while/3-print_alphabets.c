e56f112250992b26af0fe9e199c1f55887847062
LGVProd <lgveira@hotmail.com>
2023-06-16 10:17:40 -0700

    Added program to print the alphabet twice in a row

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,7 @@
 #include <time.h>
 /**
  * main - main function
- * Print the alphabet in lowercase, followed by a new line
+ * Print the alphabet in lowercase, then in uppercase, followed by a new line
  * Return: 0
  */
 int main(void)
@@ -11,7 +11,11 @@ int main(void)
 	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-
+	{
+		putchar(alphabet);
+	}
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+	{
 		putchar(alphabet);
 	}
 	putchar('\n');
--- variables_if_else_while/README.md
+++ variables_if_else_while/README.md
@@ -2,5 +2,6 @@
 <li>Added C program that prints the value of a number (stored in a variable), and its sign</li>
 <li>Added C program that prints the last digit of a number stored in a variable</li>
 <li>Added C program that prints the alphabet in lowercase, followed by a new line</li>
+<li>Added C program that prints the alphabet in lowercase and in uppercase, followed by a new line</li>
 
 </ol>
