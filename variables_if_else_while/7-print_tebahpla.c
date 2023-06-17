01a066aeacc58311a4b59f090f674d9051291345
LGVProd <lgveira@hotmail.com>
2023-06-17 14:56:39 -0700

    Added program to print the alphabet backwards

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * Print the alphabet in lowercase AND backwards, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet <= 'a'; alphabet++)
+
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
