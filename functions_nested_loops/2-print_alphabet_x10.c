ba986db6ba48323a2dd11d218a4b625bde593758
LGVProd <lgveira@hotmail.com>
2023-06-18 13:31:58 -0700

    Added program to print the alphabet 10 times in a row

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - main function
+ * Print the alphabet in lowercase, ten times in a row,
+ * followed by a new line.
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int counter;
+	char alphabet;
+
+	for (counter = 0; counter <= 10; counter++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+		_putchar('\n');
+	}
+}
--- functions_nested_loops/README.md
+++ functions_nested_loops/README.md
@@ -1,5 +1,6 @@
 <ol start="0">
 <li>Added C program that prints "\_putchar", followed by a new line</li>
 <li>Added C program that prints the alphabet in lowercase, followed by a new line</li>
+<li>Added C program that prints the alphabet in lowercase, ten times in a row</li>
 
 </ol>
